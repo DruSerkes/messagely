61cb27de7d061f4ceb634145bd42772829932322
Dru Serkes <DruSerkes@gmail.com>
2020-07-17 19:07:43 -0400

    add User register, authenticate, and updateLoginTimestamp

--- models/user.js
+++ models/user.js
@@ -1,31 +1,70 @@
 /** User class for message.ly */
 
-
+const { DB_URI, BCRYPT_WORK_FACTOR } = require('../config');
+const bcrypt = require('bcrypt');
+const ExpressError = require('../expressError');
 
 /** User of the site. */
 
 class User {
-
-  /** register new user -- returns
+	/** register new user -- returns
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+	static async register({ username, password, first_name, last_name, phone }) {
+		if (!username || !password) throw new ExpressError('username/password required', 400);
+		const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+		const result = await db.query(
+			`INSERT INTO users(
+          username,
+          password,
+          first_name,
+          last_name, 
+          phone)
+          VALUES ($1, $2, $3, $4, $5)
+          RETURNING id, username, password, first_name AS firstName, last_name AS lastName, phone
+        `,
+			[ username, hashedPassword, first_name, last_name, phone ]
+		);
+		const user = result.rows[0];
+		return user;
+	}
 
-  /** Authenticate: is this username/password valid? Returns boolean. */
+	/** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+	static async authenticate(username, password) {
+		if (!username || !password) throw new ExpressError('username/password required', 400);
+		const result = await db.query(
+			`SELECT id, username, password
+        FROM users
+        WHERE username = $1
+        `,
+			[ username ]
+		);
+		if (!result.rows.length) throw new ExpressError('User not found', 404);
+		const user = result.rows[0];
+		return await bcrypt.compare(password, user.password);
+	}
 
-  /** Update last_login_at for user */
+	/** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+	static async updateLoginTimestamp(username) {
+		if (!username) throw new ExpressError('username required', 400);
+		const result = await db.query(
+			`UPDATE users
+      SET last_login_at = current_timestamp
+      WHERE username = $1
+      `,
+			[ username ]
+		);
+	}
 
-  /** All: basic info on all users:
+	/** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
 
-  static async all() { }
+	static async all() {}
 
-  /** Get: get user by username
+	/** Get: get user by username
    *
    * returns {username,
    *          first_name,
@@ -34,9 +73,9 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) { }
+	static async get(username) {}
 
-  /** Return messages from this user.
+	/** Return messages from this user.
    *
    * [{id, to_user, body, sent_at, read_at}]
    *
@@ -44,9 +83,9 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) { }
+	static async messagesFrom(username) {}
 
-  /** Return messages to this user.
+	/** Return messages to this user.
    *
    * [{id, from_user, body, sent_at, read_at}]
    *
@@ -54,8 +93,7 @@ class User {
    *   {id, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) { }
+	static async messagesTo(username) {}
 }
-
 
 module.exports = User;
--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,14 @@
  *
  **/
 
-
 /** POST /register - register user: registers, logs in, and returns token.
  *
  * {username, password, first_name, last_name, phone} => {token}.
  *
  *  Make sure to update their last-login!
  */
+
+//  This will go in to the register route catch statement
+// if (e.code === '23505') {
+//     return next(new ExpressError('Username taken. Please pick another!', 400));
+// }
