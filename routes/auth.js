69d0f438e70de7f5e02cd22fbb94c7becc486b5a
Dru Serkes <DruSerkes@gmail.com>
2020-07-18 12:41:26 -0400

    Create auth and users routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require('../config');
+// const { Router } = require('express');
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+
+router.post('/login', async (req, res, next) => {
+	try {
+		const { username, password } = req.body;
+		if (await User.authenticate(username, password)) {
+			await User.updateLoginTimestamp(user);
+			const token = jwt.sign({ username }, SECRET_KEY);
+			return res.json({ token });
+		}
+		throw new ExpressError('Invalid username/password', 400);
+	} catch (e) {
+		return next(e);
+	}
+});
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,7 +32,18 @@
  *  Make sure to update their last-login!
  */
 
-//  This will go in to the register route catch statement
-// if (e.code === '23505') {
-//     return next(new ExpressError('Username taken. Please pick another!', 400));
-// }
+router.post('/register', async (req, res, next) => {
+	try {
+		const { username, password, first_name, last_name, phone } = req.body;
+		const user = await User.register(username, password, first_name, last_name, phone);
+		const token = jwt.sign({ username: user.username }, SECRET_KEY);
+		return res.json({ token });
+	} catch (e) {
+		if (e.code === '23505') {
+			return next(new ExpressError('Username taken. Please pick another!', 400));
+		}
+		return next(e);
+	}
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const ExpressError = require('../expressError');
+const { ensureLoggedIn, ensureCorrectUser } = require('../middleware/auth');
+const User = require('../models/user');
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
 
+router.get('/', ensureLoggedIn, async (req, res, next) => {
+	try {
+		const users = await User.all();
+		return res.json({ users });
+	} catch (e) {
+		return next(e);
+	}
+});
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
+router.get('/:username', ensureCorrectUser, async (req, res, next) => {
+	try {
+		const user = User.get(req.user.username);
+		return res.json({ user });
+	} catch (e) {
+		return next(e);
+	}
+});
 
 /** GET /:username/to - get messages to user
  *
@@ -22,6 +43,14 @@
  *
  **/
 
+router.get('/:username/to', ensureCorrectUser, async (req, res, next) => {
+	try {
+		const messages = await User.messagesTo(req.user.username);
+		return res.json({ messages });
+	} catch (e) {
+		return next(e);
+	}
+});
 
 /** GET /:username/from - get messages from user
  *
@@ -32,3 +61,13 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+router.get('/:username/from', ensureCorrectUser, async (req, res, next) => {
+	try {
+		const messages = await User.messagesFrom(req.user.username);
+		return res.json({ messages });
+	} catch (e) {
+		return next(e);
+	}
+});
+
+module.exports = router;
